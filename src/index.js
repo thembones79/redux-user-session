dd585524a2cf149cc40db305a0f7b20d075f0ead
thembones79 <michal.szumnarski@gmail.com>
2020-01-17 21:53:21 +0100

    added redux devtools

--- src/index.js
+++ src/index.js
@@ -19,7 +19,10 @@ function reducer(state = INITIAL_STATE, action) {
   }
 }
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
